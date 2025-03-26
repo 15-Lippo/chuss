c032eb6ce687094c2cf48359145792b66368fbb8
Lisprocoin <83799681+15-Lippo@users.noreply.github.com>
2025-03-26 19:09:39 +0100

    Add files via upload

--- build.sh
+++ build.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Configura l'ambiente Python
+export PYTHON_VERSION=3.9.0
+export PIP_VERSION=21.1.2
+
+# Installa Python e pip
+curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
+export PATH="$HOME/.pyenv/bin:$PATH"
+eval "$(pyenv init -)"
+pyenv install $PYTHON_VERSION
+pyenv global $PYTHON_VERSION
+
+# Aggiorna pip alla versione specificata
+python -m pip install --upgrade pip==$PIP_VERSION
+
 # Installa le dipendenze Python
 pip install -r requirements.txt
 
